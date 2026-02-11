1faaf8e1abd7315eb43575c8e7bfd081a43a948c
gabbezeira <gabrielalves.code@gmail.com>
2026-02-11 07:52:16 -0300

    fix: capitalize the first letter of the "About Us" description.

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -11,7 +11,7 @@ export function About({ illustration }) {
 			<div className="textContent">
 				<h1 className="title">Sobre Nós</h1>
 				<p className="description">
-					saudade é a prova de que o amor valeu a pena. Criamos este espaço para transformar essa saudade em homenagem e dar um lar eterno às lembranças. Aqui, as histórias, fotos e sorrisos de quem você ama continuam vivos para sempre.
+					A saudade é a prova de que o amor valeu a pena. Criamos este espaço para transformar essa saudade em homenagem e dar um lar eterno às lembranças. Aqui, as histórias, fotos e sorrisos de quem você ama continuam vivos para sempre.
 				</p>
 			</div>
 		</Container>
